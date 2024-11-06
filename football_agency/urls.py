0ebc94c9a73cd483468f12a749c8d3e1b027b617
mygrood <aamikhailovv@gmail.com>
2024-11-06 16:43:19 +0300

    Add filter,search,sort

--- football_agency/urls.py
+++ football_agency/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('teams/', views.team_list, name='team_list'),
     path('teams/create/', views.create_team, name='create_team'),
     path('players/', views.player_list, name='player_list'),
     path('players/create/', views.create_player, name='create_player'),
     path('players/<int:player_id>/', views.player_detail, name='player_detail'),
-
 ]
--- football_agency/views.py
+++ football_agency/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Team, Player
 from .forms import TeamForm, PlayerForm
 
+#ДОМАШНЯЯ СТРАНИЦА
+def home(request):
+    return render(request, 'football_agency/home.html')
 
-# Создание команды
+# СОЗДАНИЕ КОМАНДЫ
 def create_team(request):
     if request.method == 'POST':
         form = TeamForm(request.POST, request.FILES)
@@ -15,7 +18,7 @@ def create_team(request):
         form = TeamForm()
     return render(request, 'football_agency/create_team.html', {'form': form})
 
-# Создание игрока
+# СОЗДАНИЕ ИГРОКА
 def create_player(request):
     if request.method == 'POST':
         form = PlayerForm(request.POST, request.FILES)
@@ -26,17 +29,40 @@ def create_player(request):
         form = PlayerForm()
     return render(request, 'football_agency/create_player.html', {'form': form})
 
-# Отображение списка команд
+# СПИСОК КОМАНД
 def team_list(request):
     teams = Team.objects.all()
     return render(request, 'football_agency/team_list.html', {'teams': teams})
 
-# Отображение списка игроков
+# СПИСОК ИГРОКОВ
 def player_list(request):
     players = Player.objects.all()
-    return render(request, 'football_agency/player_list.html', {'players': players})
+    teams = Team.objects.all()
 
-# Функция для отображения деталей игрока
+    #параметры
+    sort = request.GET.get('sort', 'id')  # по умолчанию сортируем по 'id'
+    direction = request.GET.get('direction', 'asc')  # по умолчанию - по возрастанию
+    team = request.GET.get('team')
+    search = request.GET.get('search', '')
+
+    if team:
+        players = players.filter(id_team__id=team)
+
+    if search:
+        players = players.filter(surname__icontains=search)
+
+    if direction == 'desc':
+        sort = f'-{sort}'
+
+
+    players = players.order_by(sort)
+
+    return render(request, 'football_agency/player_list.html', {
+        'players': players,
+        'teams': teams,
+    })
+
+# ПОДРОБНОСТИ ОБ ИГРОКЕ
 def player_detail(request, player_id):
     player = get_object_or_404(Player, id=player_id)
     return render(request, 'football_agency/player_detail.html', {'player': player})
